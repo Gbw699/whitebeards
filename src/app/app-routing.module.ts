2abd7d17e7a6ba563949c6b06077ffd2921c4833
Gabriel Barimboim <gdbarimboim@gmail.com>
2024-12-10 11:53:52 -0300

    REFACTOR file names

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CoursesComponent } from './components/courses';
-import { RegisterComponent } from './components/sign-in';
-import { SignInComponent } from './components/sign-in';
+import { CatalogComponent } from './components/catalog.component';
+import { RegisterComponent } from './components/register.component';
+import { SignInComponent } from './components/register.component';
 
 const routes: Routes = [
-  { path: 'catalog', component: CoursesComponent, },
-  { path: 'users/register', component: RegisterComponent, },
-  { path: 'users/sign-in', component: SignInComponent, },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'users/register', component: RegisterComponent },
+  { path: 'users/sign-in', component: SignInComponent },
   { path: '', redirectTo: '/catalog', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
